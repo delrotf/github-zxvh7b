cdf6d211675192d0f54829cd5576fc24a4947cbd
delrotf <delrotf@users.noreply.github.com>
2023-05-23 11:26:25 +0800

    3 files added

---
+++ src/app/pills/pill-react-native/pill-react-native.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pill-react-native',
+  templateUrl: './pill-react-native.component.html',
+  styleUrls: ['./pill-react-native.component.css']
+})
+export class PillReactNativeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

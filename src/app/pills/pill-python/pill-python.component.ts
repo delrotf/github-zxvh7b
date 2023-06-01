ded9e6b359374b965a7b7dbf646563065555d80f
delrotf <delrotf@users.noreply.github.com>
2023-06-01 19:43:17 +0800

    3 files modified, 5 added, 3 deleted

---
+++ src/app/pills/pill-python/pill-python.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pill-python',
+  templateUrl: './pill-python.component.html',
+  styleUrls: ['./pill-python.component.css']
+})
+export class PillPythonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

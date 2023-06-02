497e61c3dc2a25335ebfeeabb8a173c8abfa50af
delrotf <delrotf@users.noreply.github.com>
2023-06-02 09:07:23 +0800

    3 files added

---
+++ src/app/pills/pill-tailwind-css/pill-tailwind-css.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pill-tailwind-css',
+  templateUrl: './pill-tailwind-css.component.html',
+  styleUrls: ['./pill-tailwind-css.component.css']
+})
+export class PillTailwindCssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
